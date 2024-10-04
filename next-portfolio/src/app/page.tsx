319778db5f4ad2d766f1211411d3ed5d13c7ff54
kc <77198654+tamkc@users.noreply.github.com>
2024-10-03 23:18:16 -0400

    [FEAT] Footer

--- next-portfolio/src/app/page.tsx
+++ next-portfolio/src/app/page.tsx
@@ -268,6 +268,7 @@ export default function Page() {
           </>
         )}
       </Wrapper>
+      <Footer/>
     </div>
   );
 }
---
+++ next-portfolio/src/components/ui/Footer.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import Wrapper from "../Wrapper";
+import { Linkedin, Github, Mail } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white h-24 md:h-28 relative">
+      <Wrapper>
+        <div className="border-t border-gray-700" />
+
+        <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
+          {/* Copyright and additional information */}
+          <div className="text-center md:text-left pb-2 md:pb-0">
+            <p className="text-lg">
+              &copy; {new Date().getFullYear()} Peter Tam's Portfolio. All rights
+              reserved.
+            </p>
+            <p className="text-lg text-gray-400">
+              Built with Next.js and Tailwind CSS
+            </p>
+          </div>
+
+          {/* Social Media Links */}
+          <div className="flex items-center justify-center space-x-6 md:space-x-8">
+            <Link
+              href="https://linkedin.com"
+              aria-label="LinkedIn"
+              className="hover:text-gray-300"
+            >
+              <Linkedin size={20} />
+            </Link>
+            <Link
+              href="https://github.com"
+              aria-label="GitHub"
+              className="hover:text-gray-300"
+            >
+              <Github size={20} />
+            </Link>
+            <Link
+              href="mailto:your-email@example.com"
+              aria-label="Email"
+              className="hover:text-gray-300"
+            >
+              <Mail size={20} />
+            </Link>
+          </div>
+        </div>
+      </Wrapper>
+    </footer>
+  );
+};
+
+export default Footer;
